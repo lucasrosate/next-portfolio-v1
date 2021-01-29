969d788b81f5c7fc17b26aded8c0c290c56c61d8
lucasrosate <lucas.rosate@hotmail.com>
2021-01-29 15:58:29 -0300

    Fixing erros to deploy in vercel

--- pages/components/habilities.tsx
+++ pages/components/habilities.tsx
@@ -1,8 +1,110 @@
 import React from 'react';
 
 import LanguageRender from './languagerender';
-import languages from '../files/languages';
 import style from '../../styles/habilities.module.css';
+
+const languages: ILanguage[] = [
+    // <code>React, Node.js, Django, Python, Typescript, Javascript, .NET, MySQL,MongoDB, SQLite e HTML/CSS.</code>
+
+    {
+        text: "Next.js",
+        description: "",
+        red: 74,
+        green: 233,
+        blue: 100
+
+    },
+    {
+        text: "React.js",
+        description: "",
+        red: 74,
+        green: 213,
+        blue: 255
+
+    },
+    {
+        text: "Typescript",
+        description: "",
+        red: 0,
+        green: 122,
+        blue: 204
+
+    },
+    {
+        text: "Python",
+        description: "",
+        red: 146,
+        green: 184,
+        blue: 19
+
+    },
+    {
+        text: "Django",
+        description: "",
+        red: 9,
+        green: 45,
+        blue: 31
+
+    },
+    {
+        text: "Node.js",
+        description: "",
+        red: 111,
+        green: 188,
+        blue: 78
+
+    },
+
+    {
+        text: "MongoDB",
+        description: "",
+        red: 71,
+        green: 71,
+        blue: 73
+
+    },
+    {
+        text: "MySQL",
+        description: "",
+        red: 227,
+        green: 117,
+        blue: 44
+
+    },
+    {
+        text: "Javascript",
+        description: "",
+        red: 247,
+        green: 223,
+        blue: 30
+
+    },
+    {
+        text: "SQLite",
+        description: "",
+        red: 0,
+        green: 59,
+        blue: 87
+
+    },
+    {
+        text: "HTML/CSS",
+        description: "",
+        red: 73,
+        green: 73,
+        blue: 73
+
+    },
+    {
+        text: "jQuery",
+        description: "",
+        red: 150,
+        green: 85,
+        blue: 70
+
+    },
+    
+]
 
 const Habilities: React.FC = () => {
     return (
--- pages/components/projects.tsx
+++ pages/components/projects.tsx
@@ -1,8 +1,49 @@
 import React from 'react';
-import projects from '../files/projects';
 import style from '../../styles/projects.module.css';
 import githubAccess from '../assets/githubAccess.svg';
 import ImageProject from './imageproject';
+
+
+const projects: Array<IProject> = [
+    {
+        title: "get-random-quotes",
+        images:[],
+        description: "CLI criado para gerar citações feitas por artistas com a opção de gerar e salvar em um banco de dados MySQL.",
+        technologies: ["Node.js", "MySQL", "CLI"],
+        accessLink: "https://github.com/lucasrosate/get-random-quotes",
+    },
+    {
+        title: "Fetch randomuser api",
+        images: ["https://i.imgur.com/UHfrl4c.png"],
+        description: "Esse app accessar a página https://randomuser.me/api e gera uma page com o usuário resultante.",
+        technologies: ["Next.js", "Redux", "Axios", "map-gl"],
+        accessLink: "https://github.com/lucasrosate/fetch-randomuserapi-app",
+    },
+    {
+        title: "React sudoku",
+        images: ["https://i.imgur.com/eXJ2sgE.png"],
+        description: "Jogo Sudoku criado em react.",
+        technologies: ["React.js"],
+        accessLink: "https://github.com/lucasrosate/react-sudoku",
+    },
+    {
+        title: "4life",
+        images: ["https://i.imgur.com/SP6LCU7.png", "https://i.imgur.com/LOmbGzU.png"],
+        description: "A ideia era criar um app para organizar tarefas, a vida financeira e também para cuidar da alimentação.",
+        technologies: ["React.js", "Express.js", "Node.JS", "Dropbox SDK", "MongoDB"],
+        accessLink: "https://github.com/lucasrosate/4life_app",
+    },
+    {
+        title: "Spark",
+        images: ["https://i.imgur.com/soRWjZh.png", "https://i.imgur.com/4N350d0.png", "https://i.imgur.com/4N350d0.png"],
+        description: "Site criado para a publicação de artigos e interação entre os usuários.",
+        technologies: ["Django", "jQuery", "SQLite"],
+        accessLink: "https://github.com/lucasrosate/spark_article",
+    },
+
+]
+
+
 
 const Projects: React.FC = () => {
     var [currentLink, setCurrentLink] = React.useState("");
